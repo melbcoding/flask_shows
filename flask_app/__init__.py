2c648c37f1c1e9410ca3b82abf9079cb6cf1d647
Mel B <72110634+professionalmel@users.noreply.github.com>
2022-07-12 14:17:28 -0700

    final

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
+from dotenv import load_dotenv
+load_dotenv()
 import os
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 port = os.getenv('PORT')
